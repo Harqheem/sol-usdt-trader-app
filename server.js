bf42a80700b19e13ffeb139a5d3396e6a5019d7f
Akeem Saka <temitopesakaakeem1@gmail.com>
2025-10-20 20:20:44 +0100

    ren 1

--- server.js
+++ server.js
@@ -655,4 +655,5 @@ app.get('/price', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
